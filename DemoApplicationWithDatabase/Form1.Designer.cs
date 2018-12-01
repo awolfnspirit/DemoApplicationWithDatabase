2f3dfea82c13505fdc13cbeaaafeecd8538e9228
Chris Cotton <awolfnspirit@gmail.com>
2018-12-01 15:27:09 -0700

    Setup Button Connect with code to connect to SQL DB and to display message box.

--- DemoApplicationWithDatabase/Form1.Designer.cs
+++ DemoApplicationWithDatabase/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace DemoApplicationWithDatabase
 {
-    partial class Form1
+    partial class Form_Login
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button_Connect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_Connect
+            // 
+            this.button_Connect.Location = new System.Drawing.Point(111, 87);
+            this.button_Connect.Name = "button_Connect";
+            this.button_Connect.Size = new System.Drawing.Size(101, 38);
+            this.button_Connect.TabIndex = 0;
+            this.button_Connect.Text = "Connect";
+            this.button_Connect.UseVisualStyleBackColor = true;
+            this.button_Connect.Click += new System.EventHandler(this.button_Connect_Click);
+            // 
+            // Form_Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(340, 450);
+            this.Controls.Add(this.button_Connect);
+            this.Name = "Form_Login";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button_Connect;
     }
 }
 
--- DemoApplicationWithDatabase/Form1.cs
+++ DemoApplicationWithDatabase/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -10,11 +11,22 @@ using System.Windows.Forms;
 
 namespace DemoApplicationWithDatabase
 {
-    public partial class Form1 : Form
+    public partial class Form_Login : Form
     {
-        public Form1()
+        public Form_Login()
         {
             InitializeComponent();
+        }
+
+        private void button_Connect_Click(object sender, EventArgs e)
+        {
+            string connetionString;
+            SqlConnection sql_connection;
+            connetionString = @"Data Source=WIN-50GP30FGO75;Initial Catalog=Demodb;User ID=sa;Password=demol23";
+            sql_connection = new SqlConnection(connetionString);
+            sql_connection.Open();
+            MessageBox.Show("Connection Open  !");
+            sql_connection.Close();
         }
     }
 }
